187ed9713727b0e56e7fa6e98e2733bcde86ab08
rudicgeologist <rudicgeologist@proton.me>
2023-01-05 19:56:27 +0300

    0.0

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const dba = require('./db_adapter.js');
 
 app.get('/', (req, res) => {
